fc2a7e8ff2263ba643d8b20ea770b7e7701140d0
张翔 <zhangxiang.zxx@bytedance.com>
2020-12-05 15:23:57 +0800

    complete the barcode read and write

--- src/main/java/org/App/App.java
+++ src/main/java/org/App/App.java
@@ -1,5 +1,9 @@
 package org.App;
 
+import org.zxx.Utils.Barcode;
+
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        Barcode.BookBarcode("aaaaa");
         System.out.println( "Hello World!" );
+        Barcode.readCode(new File("/Users/bytedance/Desktop/Barcode-test/abc.jpg"));
     }
 }
---
+++ src/main/java/org/zxx/Utils/Barcode.java
@@ -0,0 +1,183 @@
+package org.zxx.Utils;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.oned.Code128Writer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Barcode {
+
+    /**
+     * 条形码宽度
+     */
+    private static final int WIDTH = 300;
+
+    /**
+     * 条形码高度
+     */
+    private static final int HEIGHT = 50;
+
+    /**
+     * 加文字 条形码
+     */
+    private static final int WORDHEIGHT = 75;
+
+    /**
+     * 设置 条形码参数
+     */
+    private static Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>() {
+        private static final long serialVersionUID = 1L;
+
+        {
+            // 设置编码方式
+            put(EncodeHintType.CHARACTER_SET, "utf-8");
+        }
+    };
+
+    /**
+     * 生成 图片缓冲
+     *
+     * @param vaNumber VA 码
+     * @return 返回BufferedImage
+     * @author fxbin
+     */
+    public static BufferedImage getBarCode(String vaNumber) {
+        try {
+            Code128Writer writer = new Code128Writer();
+            // 编码内容, 编码类型, 宽度, 高度, 设置参数
+            BitMatrix bitMatrix = writer.encode(vaNumber, BarcodeFormat.CODE_128, WIDTH, HEIGHT, hints);
+            return MatrixToImageWriter.toBufferedImage(bitMatrix);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void readCode(File file) {
+        try {
+            BufferedImage image = ImageIO.read(file);
+            if (image == null) {
+                return;
+            }
+            LuminanceSource source = new BufferedImageLuminanceSource(image);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+            Map<DecodeHintType, Object> hints = new HashMap<>();
+            hints.put(DecodeHintType.CHARACTER_SET, "GBK");
+            hints.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+            hints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+
+            Result result = new MultiFormatReader().decode(bitmap, hints);
+            System.out.println("条形码内容: " + result.getText());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 把带logo的二维码下面加上文字
+     *
+     * @param image 条形码图片
+     * @param words 文字
+     * @return 返回BufferedImage
+     * @author fxbin
+     */
+    public static BufferedImage insertWords(BufferedImage image, String words) {
+        // 新的图片，把带logo的二维码下面加上文字
+        if (words!=null&&words.length()>0) {
+
+            BufferedImage outImage = new BufferedImage(WIDTH, WORDHEIGHT, BufferedImage.TYPE_INT_RGB);
+
+            Graphics2D g2d = outImage.createGraphics();
+
+            // 抗锯齿
+            setGraphics2D(g2d);
+            // 设置白色
+            setColorWhite(g2d);
+
+            // 画条形码到新的面板
+            g2d.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+            // 画文字到新的面板
+            Color color = new Color(0, 0, 0);
+            g2d.setColor(color);
+            // 字体、字型、字号
+            g2d.setFont(new Font("微软雅黑", Font.PLAIN, 18));
+            //文字长度
+            int strWidth = g2d.getFontMetrics().stringWidth(words);
+            //总长度减去文字长度的一半  （居中显示）
+            int wordStartX = (WIDTH - strWidth) / 2;
+            //height + (outImage.getHeight() - height) / 2 + 12
+            int wordStartY = HEIGHT + 20;
+
+            // 画文字
+            g2d.drawString(words, wordStartX, wordStartY);
+            g2d.dispose();
+            outImage.flush();
+            return outImage;
+        }
+        return null;
+    }
+
+    /**
+     * 设置 Graphics2D 属性  （抗锯齿）
+     *
+     * @param g2d Graphics2D提供对几何形状、坐标转换、颜色管理和文本布局更为复杂的控制
+     */
+    private static void setGraphics2D(Graphics2D g2d) {
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_DEFAULT);
+        Stroke s = new BasicStroke(1, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER);
+        g2d.setStroke(s);
+    }
+
+    /**
+     * 设置背景为白色
+     *
+     * @param g2d Graphics2D提供对几何形状、坐标转换、颜色管理和文本布局更为复杂的控制
+     */
+    private static void setColorWhite(Graphics2D g2d) {
+        g2d.setColor(Color.WHITE);
+        //填充整个屏幕
+        g2d.fillRect(0, 0, 600, 600);
+        //设置笔刷
+        g2d.setColor(Color.BLACK);
+    }
+
+    public static void BookBarcode(String bookid) {
+        try {
+            BufferedImage image = insertWords(getBarCode(bookid), bookid);
+            File temp = new File("/Users/bytedance/Desktop/Barcode-test/abc.jpg");
+            if(temp.exists()) {
+                ImageIO.write(image, "jpg", temp);
+                System.out.println("can write");
+            }else {
+                temp.createNewFile();
+                ImageIO.write(image, "jpg", temp);
+                System.out.println("can write two");
+            }
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            ImageIO.write(image, "jpg", baos);
+//            baos.flush();
+//            //使用toByteArray()方法转换成字节数组
+//            byte[] imageInByte = baos.toByteArray();
+//            String msg = Base64.getEncoder().encodeToString(imageInByte);
+//            baos.close();
+//            return msg;
+        } catch (IOException e) {
+            System.out.println("e = " + e);
+        }
+    }
+}
